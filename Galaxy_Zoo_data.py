d29619827a0453a6f40cfc19d048984092c79304
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-06-21 13:02:38 -0700

    merge data download file

--- Galaxy_Zoo_data.py
+++ Galaxy_Zoo_data.py
@@ -1,36 +1,12 @@
-
-# coding: utf-8
-
-# In[5]:
-
-
 import pandas as pd
 import urllib.request
 
-
-# In[7]:
-
-
-n = 2 #number of files to download
+n = 2  # number of files to download
 data = pd.read_csv('Book.csv')
-data = pd.DataFrame(data);
-
-
-# In[8]:
-
+data = pd.DataFrame(data)
 
 data.head()
 
-
-# In[9]:
-
-
-cd ~/Documents/GitHub/Infinity_Categorizer
-
-
-# In[11]:
-
-
 for i in range(n):#np.size(data.OBJID)):
-    urllib.request.urlretrieve('http://skyserver.sdss.org/dr14/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Explore.Image&ra={0}&dec={1}&scale=.2&width=200&height=200'.format(data.RA[i], data.DEC[i]),'Img_{0}_{1}.jpeg'.format(i,data.OBJID[i]))
+    urllib.request.urlretrieve('http://skyserver.sdss.org/dr14/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Explore.Image&ra={0}&dec={1}&scale=.2&width=200&height=200'.format(data.RA[i], data.DEC[i]),'Img_{0}.jpeg'.format(data.OBJID[i]))
 
