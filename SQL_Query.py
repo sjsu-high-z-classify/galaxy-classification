20c65eb9d5650d1d421df6083775733e1007d412
hthummar30 <hthummar30@gmail.com>
2018-07-16 16:42:45 -0700

    Update on SQL Query
    I am using SciServer (python package) instead of that link we used to download all the tables.

--- SQL_Query.py
+++ SQL_Query.py
@@ -7,48 +7,27 @@
 import pandas as pd
 import numpy as np
 import mechanicalsoup  as ms
+from SciServer import Authentication, CasJobs
 import time
 
 
 # In[2]:
 
 
-url = "http://skyserver.sdss.org/dr14/en/tools/search/sql.aspx"
-br = ms.StatefulBrowser()
+Authentication_loginName = 'hiren'
+Authentication_loginPassword = 'Asdfg@123'
+token = Authentication.login(Authentication_loginName, Authentication_loginPassword)
 
 
 # In[3]:
 
 
-def SDSS_select(sql):
-    '''
-    input:  string with a valid SQL query
-    output: csv
-    source: http://balbuceosastropy.blogspot.com/2013/10/an-easy-way-to-make-sql-queries-from.html
-    '''
-    br.open(url)
-    br.select_form("[name=sql]")
-    br['cmd'] = sql
-    br["format"]="csv"
-    response = br.submit_selected()
-    return response.text
-    
-def writer(filename, data):
-    # writes data to a file
-    f = open(filename, 'w')
-    f.write(data)
-    f.close()
-    return writer
-
 def database(n):
-    s = "SELECT TOP {}         specobjid, objid as objid8, ra, dec,         spiral, elliptical, uncertain     FROM ZooSpec".format(n)
-
-    SDSS = SDSS_select(s)
-    writer('data_GZ1.csv', SDSS)
+    query = "SELECT TOP {} specobjid, objid as objid8, ra, dec, spiral, elliptical, uncertain FROM ZooSpec".format(n)
+    responseStream = CasJobs.executeQuery(query, "DR14", format="pandas")
     
-    data = pd.read_csv('data_GZ1.csv',header = 1)
-    data = pd.DataFrame(data)
-    
+    data = pd.DataFrame(responseStream)
+#     print("\n---Query---\n{}\n---Result---\n{}".format(query, responseStream))
     data['Gtype'] = ''
     
     for i in range(np.size(data.specobjid)):
@@ -64,7 +43,7 @@ def database(n):
     return database
 
 
-# In[8]:
+# In[4]:
 
 
 database(1000)
