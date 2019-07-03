2761d085d913c3ba733ec7d9713a6eece3d06f82
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-07-03 16:36:16 -0700

    Changes to the run file so that we can use gpu computing. Keras supports this automatically as long as the right packages are installed (included in the environment.yml file).

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-#SBATCH --partition=nodes
+#SBATCH --partition=gpus
 #SBATCH --nodes=1                                 # Nodes requested
-#SBATCH --ntasks-per-node=16                       # taskes per node
-#SBATCH --mem=23000                               # memory per node (MB)
-#SBATCH --job-name="train_test"
-#SBATCH --output=OUT.out                    # send stdout to outfile
-#SBATCH --error=ERR.err                     # send stderr to errfile
-#SBATCH --time=4:00:00                            # time requested in day-hour:minute:second
-#SBATCH --mail-user=user.email@sjsu.edu
+#SBATCH --gres=gpu
+#SBATCH --job-name=galnet
+#SBATCH --output=galnet.out                    # send stdout to outfile
+#SBATCH --error=galnet.err                     # send stderr to errfile
+#SBATCH --time=10-00:00:00                            # time requested in day-hour:minute:second
+#SBATCH --mail-user=james.casey-clyde@sjsu.edu
 #SBATCH --mail-type=ALL
 
-cd ~/path/to/source/
-module load intel-python3
-python3 galnet.py -u '***' -p '***' -r 10000
+conda activate galnet-gpu
+python cnn.py
