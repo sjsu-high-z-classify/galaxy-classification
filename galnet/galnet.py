b31c55ca7b957459351f7805d237426be5550855
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-07-08 17:28:56 -0700

    Adopting the agreement threshold used by Dominguez-Sanchez et al. 2018

--- galnet/galnet.py
+++ galnet/galnet.py
@@ -34,7 +34,7 @@ def main(argv):
     # import our dataset, which is a pandas dataframe containing path
     # information to the actual image data
     gz2 = pd.read_hdf(os.path.join(argv.DATA, 'gz2.h5'))
-    print(gz2['imgpath'][0])
+    gz2 = gz2[gz2['a01'] >= .3]  # we adopt the agreement threshold of DS18
 
     # choose the questions we want to classify. note that the number
     # of columns here should match the number of output layers in
