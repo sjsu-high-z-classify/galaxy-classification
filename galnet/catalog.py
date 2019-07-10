fa98bc303c828693785cca9c38592b71da637518
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-07-10 16:37:49 -0700

    Added GPLv3 statement to module files.

--- galnet/catalog.py
+++ galnet/catalog.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
+# Module to build catalog of GZ2 galaxies for use with GalNet.
+# Copyright (C) 2018-2019  J. Andrew Casey-Clyde and Hiren Thummar.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import argparse
