339252ecddec82d5803dbadca24b9afc826309eb
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2019-07-18 15:16:14 -0700

    typo

--- galnet/model.py
+++ galnet/model.py
@@ -59,7 +59,7 @@ def model_builder(input_dim):
 #    t01 = Dense(2048, activation='relu')(cnn)
 #    t01 = Dropout(.5)(t01)
 
-    t01 = Dense(64, activation='relu')(t01)
+    t01 = Dense(64, activation='relu')(cnn)
     t01 = Dropout(.5)(t01)
     t01_out = Dense(3, activation='softmax', name='t01')(t01)
 
